52dbedca9a13583b309eea52f01f7e8c3fe1ad79
Pragati Kabra <56672534+Pragati2001@users.noreply.github.com>
2022-03-26 00:16:06 +0530

    Delete skribbl/frontend directory

--- skribbl/frontend/src/components/Next_Page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Next_Page() {
-  return (
-    <div>Next_Page</div>
-  )
-}
--- skribbl/frontend/src/components/Scribbl_heading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import '../styles/start.css';
-export default function Scribbl_heading() {
-  return (
-    <div className='scribbl-heading'>SCRIBBL</div>
-  )
-}
