5ebab1452adadd4dd71946ed761bac0d44a623ec
Pragati Kabra <56672534+Pragati2001@users.noreply.github.com>
2022-03-26 19:17:35 +0530

    Update Canvas.js

--- skribbl/frontend/src/components/Canvas.js
+++ skribbl/frontend/src/components/Canvas.js
@@ -1,99 +1,75 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Menu from "./Menu";
+import "../styles/canvas_style.css";
 
-const colors = ["red", "green", "yellow", "black", "blue"];
+function Canvas() {
+const canvasRef = useRef(null);
+const ctxRef = useRef(null);
+const [isDrawing, setIsDrawing] = useState(false);
+const [lineWidth, setLineWidth] = useState(5);
+const [lineColor, setLineColor] = useState("black");
+const [lineOpacity, setLineOpacity] = useState(0.1);
 
-export default function Canvas() {
-  const canvasRef = useRef(null);
-  const ctx = useRef(null);
+// Initialization when the component
+// mounts for the first time
+useEffect(() => {
+	const canvas = canvasRef.current;
+	const ctx = canvas.getContext("2d");
+	ctx.lineCap = "round";
+	ctx.lineJoin = "round";
+	ctx.globalAlpha = lineOpacity;
+	ctx.strokeStyle = lineColor;
+	ctx.lineWidth = lineWidth;
+	ctxRef.current = ctx;
+}, [lineColor, lineOpacity, lineWidth]);
 
-  const [selectedColor, setSelectedColor] = useState(colors[0]);
-  const [mouseDown, setMouseDown] = useState(false);
-  const [lastPosition, setPosition] = useState({
-    x: 0,
-    y: 0
-  });
+// Function for starting the drawing
+const startDrawing = (e) => {
+	ctxRef.current.beginPath();
+	ctxRef.current.moveTo(
+	e.nativeEvent.offsetX,
+	e.nativeEvent.offsetY
+	);
+	setIsDrawing(true);
+};
 
-  useEffect(() => {
-    if (canvasRef.current) {
-      ctx.current = canvasRef.current.getContext('2d');
-    }
-  }, []);
+// Function for ending the drawing
+const endDrawing = () => {
+	ctxRef.current.closePath();
+	setIsDrawing(false);
+};
 
-  const draw = useCallback((x, y) => {
-    if (mouseDown) {
-      ctx.current.beginPath();
-      ctx.current.strokeStyle = selectedColor;
-      ctx.current.lineWidth = 10;
-      ctx.current.lineJoin = 'round';
-      ctx.current.moveTo(lastPosition.x, lastPosition.y);
-      ctx.current.lineTo(x, y);
-      ctx.current.closePath();
-      ctx.current.stroke();
+const draw = (e) => {
+	if (!isDrawing) {
+	return;
+	}
+	ctxRef.current.lineTo(
+	e.nativeEvent.offsetX,
+	e.nativeEvent.offsetY
+	);
+	
+	ctxRef.current.stroke();
+};
 
-      setPosition({
-        x,
-        y
-      })
-    }
-  }, [lastPosition, mouseDown, selectedColor, setPosition])
-
-  const download = async () => {
-    const image = canvasRef.current.toDataURL('image/png');
-    const blob = await (await fetch(image)).blob();
-    const blobURL = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = blobURL;
-    link.download = "image.png";
-    link.click();
-  }
-
-  const clear = () => {
-    ctx.current.clearRect(0, 0, ctx.current.canvas.width, ctx.current.canvas.height)
-  }
-
-  const onMouseDown = (e) => {
-    setPosition({
-      x: e.pageX,
-      y: e.pageY
-    })
-    setMouseDown(true)
-  }
-
-  const onMouseUp = (e) => {
-    setMouseDown(false)
-  }
-
-  const onMouseMove = (e) => {
-    draw(e.pageX, e.pageY)
-  }
-
-  return (
-    <div >
-      <canvas
-        style={{
-          border: "1px solid #000"
-        }}
-        width={400}
-        height={400}
-        ref={canvasRef}
-        onMouseDown={onMouseDown}
-        onMouseUp={onMouseUp}
-        onMouseLeave={onMouseUp}
-        onMouseMove={onMouseMove}
-      />
-      <br />
-      <select
-        value={selectedColor}
-        onChange={(e) => setSelectedColor(e.target.value)}
-      >
-        {
-          colors.map(
-            color => <option key={color} value={color}>{color}</option>
-          )
-        }
-      </select>
-      <button onClick={clear}>Clear</button>
-      <button onClick={download}>Download</button>
-    </div>
-  );
+return (
+	<div className="canvas_prac">
+	<div className="draw-area">
+		<Menu
+		setLineColor={setLineColor}
+		setLineWidth={setLineWidth}
+		setLineOpacity={setLineOpacity}
+		/>
+		<canvas
+		onMouseDown={startDrawing}
+		onMouseUp={endDrawing}
+		onMouseMove={draw}
+		ref={canvasRef}
+		width={`700px`}
+		height={`720px`}
+		/>
+	</div>
+	</div>
+);
 }
+
+export default Canvas;
